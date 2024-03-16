10eb0a1d06c16cfa89cd39ba68c94394ebced70d
03-JS <pyjoso3vitaslim@gmail.com>
2024-03-16 19:54:38 +0100

    Add project files.

---
+++ KeepEnemyPower/Patches/EnemyAIPatch.cs
@@ -0,0 +1,70 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace KeepEnemyPower.Patches
+{
+    [HarmonyPatch(typeof(EnemyAI))]
+    internal class EnemyAIPatch
+    {
+        [HarmonyPatch("SubtractFromPowerLevel")]
+        [HarmonyPrefix]
+        static void CheckPreviousPower(EnemyAI __instance, ref bool ___removedPowerLevel)
+        {
+            if (!___removedPowerLevel)
+            {
+                if (__instance.enemyType.isDaytimeEnemy)
+                {
+                    Plugin.LogToConsole("Previous Daytime Enemy Power -> " + RoundManager.Instance.currentDaytimeEnemyPower);
+                    return;
+                }
+                if (__instance.isOutside)
+                {
+                    Plugin.LogToConsole("Previous Outside Enemy Power -> " + RoundManager.Instance.currentOutsideEnemyPower);
+                    return;
+                }
+                Plugin.LogToConsole("Previous Inside Enemy Power -> " + RoundManager.Instance.currentEnemyPower);
+            }
+        }
+
+        [HarmonyPatch("SubtractFromPowerLevel")]
+        [HarmonyPostfix]
+        static void KeepPowerLevel(EnemyAI __instance, ref bool ___removedPowerLevel)
+        {
+            if (___removedPowerLevel)
+            {
+                if (__instance.enemyType.isDaytimeEnemy)
+                {
+                    if (Plugin.keepOutdoorsPower.Value)
+                    {
+                        ___removedPowerLevel = false;
+                        RoundManager.Instance.currentDaytimeEnemyPower = Mathf.Max(RoundManager.Instance.currentDaytimeEnemyPower + __instance.enemyType.PowerLevel, 0);
+                    }
+                    Plugin.LogToConsole("Current Daytime Enemy Power -> " + RoundManager.Instance.currentDaytimeEnemyPower);
+                    return;
+                }
+                if (__instance.isOutside)
+                {
+                    if (Plugin.keepOutdoorsPower.Value)
+                    {
+                        ___removedPowerLevel = false;
+                        RoundManager.Instance.currentOutsideEnemyPower = Mathf.Max(RoundManager.Instance.currentOutsideEnemyPower + __instance.enemyType.PowerLevel, 0);
+                    }
+                    Plugin.LogToConsole("Current Outside Enemy Power -> " + RoundManager.Instance.currentOutsideEnemyPower);
+                    return;
+                }
+                if (Plugin.keepIndoorsPower.Value)
+                {
+                    ___removedPowerLevel = false;
+                    // RoundManager.Instance.cannotSpawnMoreInsideEnemies = true;
+                    RoundManager.Instance.currentEnemyPower = Mathf.Max(RoundManager.Instance.currentEnemyPower + __instance.enemyType.PowerLevel, 0);
+                }
+                Plugin.LogToConsole("Current Inside Enemy Power -> " + RoundManager.Instance.currentEnemyPower);
+            }
+        }
+    }
+}
---
+++ KeepEnemyPower/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using KeepEnemyPower.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepEnemyPower
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "JS03.KeepEnemyPower";
+        private const string modName = "Keep Enemy Power";
+        private const string modVersion = "1.0.0";
+
+        // Config values
+        public static ConfigEntry<bool> keepIndoorsPower;
+        public static ConfigEntry<bool> keepOutdoorsPower;
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static Plugin Instance;
+        internal static ManualLogSource mls;
+
+        void Awake()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Keep Enemy Power is now active");
+
+            GenerateConfigValues();
+            harmony.PatchAll(typeof(EnemyAIPatch));
+        }
+
+        internal void GenerateConfigValues()
+        {
+            keepIndoorsPower = Config.Bind(
+                "General", // Config section
+                "Keep Indoors Power", // Key of this config
+                true, // Default value
+                "Keeps the indoors enemy power when an indoor enemy dies" // Description
+            );
+
+            keepOutdoorsPower = Config.Bind(
+                "General", // Config section
+                "Keep Outdoors Power", // Key of this config
+                true, // Default value
+                "Keeps the outdoors enemy power when an outdoor enemy dies" // Description
+            );
+        }
+
+        public static void LogToConsole(string message, string logType = "")
+        {
+            switch (logType.ToLower())
+            {
+                case "warn":
+                    mls.LogWarning(message);
+                    break;
+                case "error":
+                    mls.LogError(message);
+                    break;
+                default:
+                    mls.LogInfo(message);
+                    break;
+            }
+        }
+    }
+}
